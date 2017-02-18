fa96171fd7eaf3862a21ac7f86a25287c8e920ca
vslbros <vinulasrado@gmail.com>
2017-02-18 10:47:34 +0530

    Removed extracter

--- extraction_thehindu/django_project/newsdb/urls.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from __future__ import unicode_literals
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
--- extraction_thehindu/django_project/newsdb/views.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-from __future__ import unicode_literals
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Welcome news-aggregator-analyzer.")
-
