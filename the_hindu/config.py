6cf05d16e74abdcdf1918ab6bba730851e51d0b5
vslbros <vinulasrado@gmail.com>
2017-02-23 11:49:34 +0530

    Adding the Hindu Extractor and Parser

---
+++ the_hindu/config.py
@@ -0,0 +1,15 @@
+
+class ConfigThehindu():
+	
+	def get_server_list(self):
+		server_links_list=[# Add the list of servers]
+		return server_links_list
+
+def main():
+	config_obj=ConfigThehindu()
+	config_obj.get_server_list()
+	return 0
+
+if __name__ == '__main__':
+	main()
+
