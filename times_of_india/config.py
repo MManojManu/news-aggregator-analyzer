00bc4206805382e44d9eda3ece0a945ffb5c84e6
akshay karapurkar <karapurkar.akshay@gmail.com>
2017-02-23 15:21:49 +0530

    deleted

--- times_of_india/config.py
+++
@@ -1,3 +0,0 @@
-
-start_proxy_str='http://username:password@ip address:port number'
-proxy_server_list=['http://username:password@ip address:port number','http://username:password@ip address:port number','http://username:password@ip address:port number','http://username:password@ip address:port number']
