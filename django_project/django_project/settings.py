9cb49e03d21be049bc80d21f69642fb3208b876b
MManojManu <manojmanu15319@gmail.com>
2017-02-27 15:44:38 +0530

    updated the django settings file

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-
+#django settings file
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
