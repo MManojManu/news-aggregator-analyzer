e518da1e7f9e501c2d7cd985671f44c0b0cb67a1
MManojManu <manojmanu15319@gmail.com>
2017-03-22 19:39:51 +0530

    Added Search Folder

---
+++ search/script/sphinx_indexer.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+import os
+
+"""Indexer file"""
+class Process(object):
+
+    def start(self):
+        sudoPassword = ''
+	command = 'sudo service sphinxsearch stop'
+	os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+	print('\nStopped the Server')
+	command = 'sudo /var/lib/sphinxsearch/data/*'
+	os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+	print("Deleted the old Index files")
+        command = 'sudo indexer --config /etc/sphinxsearch/sphinx.conf --all'
+        os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+	command = 'sudo service sphinxsearch start'
+	os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+	print('\nStarted the Server')
+
+obj_process = Process()
+obj_process.start()
