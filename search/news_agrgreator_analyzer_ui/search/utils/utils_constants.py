da7e6296ef6cbd1a35380a7e78516ebb585cc0a7
MManojManu <manojmanu15319@gmail.com>
2017-03-22 19:32:11 +0530

    Removed Search

--- search/news_agrgreator_analyzer_ui/search/utils/utils_constants.py
+++
@@ -1,6 +0,0 @@
-FACET_MAP = {
-            "location": "resolved_location_name",
-            "news_type": "resolved_news_type_name",
-            "source": "source_name",
-        }
-FORMATOR = lambda x: "'" + x + "'"
